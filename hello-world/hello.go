df98d67324b49b75526fa0750475fcbad216ca89
Nobuchika Tanaka <codequokka@gmail.com>
2022-10-23 08:41:03 +0900

    Add Hello()

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+// Hello returns a greeting.
+func Hello() string {
+	return "Hello, world"
+}
+
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println(Hello())
 }
