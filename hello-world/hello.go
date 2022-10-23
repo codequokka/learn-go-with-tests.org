2ab8a039d874aa25c49c6c58673b0e000afd96c8
Nobuchika Tanaka <codequokka@gmail.com>
2022-10-23 18:57:13 +0900

    Refactor Hello()

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -14,13 +14,16 @@ func Hello(name, language string) string {
 		name = "world"
 	}
 
-	if language == spanish {
-		return spanishHelloPrefix + name
-	} else if language == french {
-		return frenchHelloPrefix + name
+	prefix := englishHelloPrefix
+
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
 	}
 
-	return englishHelloPrefix + name
+	return prefix + name
 }
 
 func main() {
