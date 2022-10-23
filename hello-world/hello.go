285ed09dbe2e1e591963a0e923f890ebeae70d0e
Nobuchika Tanaka <codequokka@gmail.com>
2022-10-23 11:20:44 +0900

    Add a name arg to Hello()

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,11 +2,11 @@ package main
 
 import "fmt"
 
-// Hello returns a greeting.
-func Hello() string {
-	return "Hello, world"
+// Hello returns a personalized greeting.
+func Hello(name string) string {
+	return "Hello, " + name
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("world"))
 }
