fb24648c5225b0a830c4cd8e8c1fe990f3b08230
Nobuchika Tanaka <codequokka@gmail.com>
2022-10-24 06:37:20 +0900

    Add ExampleAdd()

--- integers/add_test.go
+++ integers/add_test.go
@@ -1,6 +1,9 @@
 package integers
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	got := Add(1, 2)
@@ -9,4 +12,10 @@ func TestAdd(t *testing.T) {
 	if got != want {
 		t.Errorf("got: %d want: %d", got, want)
 	}
+}
+
+func ExampleAdd() {
+	sum := Add(2, 3)
+	fmt.Println(sum)
+	// Output: 5
 }
